33b44f5a987ae486ea2ee98c5e6343011cf19ba6
Lis-cyber <lisjardim6@gmail.com>
2020-12-26 20:17:37 -0300

    Changes Add Cart Item

--- src/App.js
+++ src/App.js
@@ -11,16 +11,20 @@ class App extends React.Component {
     super()
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: ""
     }
+  }
+  createOrder = (order) => {
+    alert("Need to save order for "+ order)
   }
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({
       cartItems: cartItems.filter((x) => x._id !== product._id),
     });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !== product._id)))
   };
 
   addToCart = (product) => {
@@ -36,6 +40,7 @@ class App extends React.Component {
         cartItems.push({...product, count: 1})
       }
     this.setState({ cartItems })
+    localStorage.setItem("cartItems", JSON.stringify(cartItems))
   }
   
   sortProducts = (event) => {
@@ -47,7 +52,7 @@ class App extends React.Component {
         ((a.price > b.price)? 1 : -1):
         sort === "highest"?
         ((a.price < b.price)? 1 : -1):
-        ((a._id > b._id)? 1:-1)
+        ((a._id > b._id)? 1 : -1)
 
       ))
     }))
